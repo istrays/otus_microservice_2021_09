b7a09586c542639de8d262bba6353031515d4260
straysizmailova@gmail.com <1esT9i6ag>
2021-12-30 11:41:04 +0300

    Add user service with kubernetes manifests

---
+++ hw02-kubernetes-3/users/src/main/java/com/strays/users/UserEntity.java
@@ -0,0 +1,37 @@
+package com.strays.users;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
